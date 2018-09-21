3b563be62fa0521a4dc998ecc15c7fc4aadaa3b2
dambusm <willschaller@gmail.com>
2018-09-21 23:36:23 +0100

    Edit build script to install Shotcut from github

--- build/index.sh
+++ build/index.sh
@@ -8,37 +8,13 @@ if [ $? -ne 0 ]; then
 fi
 
 download () {
-	curl -L -# -A 'https://github.com/eugeneware/ffmpeg-static' -o $2 $1
+	curl -L -# -A 'https://github.com/dambusm/shotcut-static' -o $2 $1
 }
 
-echo 'windows x64'
-echo '  downloading from ffmpeg.zeranoe.com'
-download 'https://ffmpeg.zeranoe.com/builds/win64/static/ffmpeg-latest-win64-static.zip' win32-x64.zip
-echo '  extracting'
-unzip -o -d ../bin/win32/x64 -j win32-x64.zip '**/ffmpeg.exe'
-
-echo 'windows ia32'
-echo '  downloading from ffmpeg.zeranoe.com'
-download 'https://ffmpeg.zeranoe.com/builds/win32/static/ffmpeg-latest-win32-static.zip' win32-ia32.zip
-echo '  extracting'
-unzip -o -d ../bin/win32/ia32 -j win32-ia32.zip '**/ffmpeg.exe'
-
+RELEASE=$(curl -s https://api.github.com/repos/mltframework/shotcut/releases | grep browser_download_url | grep '[.]bz2' | head -n 1 | cut -d '"' -f 4)
 echo 'linux x64'
-echo '  downloading from johnvansickle.com'
-download 'https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-64bit-static.tar.xz' linux-x64.tar.xz
+echo $RELEASE
+echo '  downloading'
+download $RELEASE linux-x64.tar.bz2
 echo '  extracting'
-$tar_exec -x -C ../bin/linux/x64 --strip-components 1 -f linux-x64.tar.xz --wildcards '*/ffmpeg'
-
-echo 'linux ia32'
-echo '  downloading from johnvansickle.com'
-download 'https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-32bit-static.tar.xz' linux-ia32.tar.xz
-echo '  extracting'
-$tar_exec -x -C ../bin/linux/ia32 --strip-components 1 -f linux-ia32.tar.xz --wildcards '*/ffmpeg'
-
-# find latest version
-LATEST_OSX=$(curl -s https://evermeet.cx/pub/ffmpeg/ | grep ffmpeg | grep .7z | head -1 | cut -d'"' -f 2)
-
-echo 'darwin x64'
-echo '  downloading from evermeet.cx'
-download "https://evermeet.cx/pub/ffmpeg/${LATEST_OSX}" darwin-x64-ffmpeg.7z
-7zr e -y -bd -o../bin/darwin/x64 darwin-x64-ffmpeg.7z >/dev/null
+$tar_exec -xvj -C ../bin/linux/x64  -f linux-x64.tar.bz2 --strip-components 2
