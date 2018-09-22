35f548a261f0dd98fa2293b3a010d3c788d63b14
dambusm <willschaller@gmail.com>
2018-09-22 13:23:46 +0100

    Provide user access to install binary paths

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
-var os = require('os')
-var path = require('path')
+const os = require('os');
+const path = require('path');
 
-var platform = os.platform()
-if (platform !== 'linux' && platform !== 'darwin' && platform !== 'win32') {
-  console.error('Unsupported platform.')
-  process.exit(1)
+const platform = os.platform();
+if (platform !== 'linux') {
+  console.error('Unsupported platform.');
+  process.exit(1);
 }
 
-var arch = os.arch()
-if (platform === 'darwin' && arch !== 'x64') {
-  console.error('Unsupported architecture.')
-  process.exit(1)
+const arch = os.arch();
+if (arch !== 'x64') {
+  console.error('Unsupported architecture.');
+  process.exit(1);
 }
 
-var ffmpegPath = path.join(
-  __dirname,
-  'bin',
-  platform,
-  arch,
-  platform === 'win32' ? 'ffmpeg.exe' : 'ffmpeg'
-)
+const dir = path.join(__dirname, 'bin', platform, arch);
 
-exports.path = ffmpegPath;
+const ffmpeg = path.join(dir, 'ffmpeg');
+const ffplay = path.join(dir, 'ffplay');
+const ffprobe = path.join(dir, 'ffprobe');
+const melt = path.join(dir, 'melt');
+const qmelt = path.join(dir, 'qmelt');
+const shotcut = path.join(dir, 'shotcut');
+
+module.exports = { ffmpeg, ffplay, ffprobe, melt, qmelt, shotcut };
