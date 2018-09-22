96985290f5e6e9a0634c5c331bd75489a8b655ee
dambusm <willschaller@gmail.com>
2018-09-22 13:24:16 +0100

    Add tests for checking binaries are installed

--- test/index.js
+++ test/index.js
@@ -1,9 +1,39 @@
 var it = require('tape'),
-    fs = require('fs'),
-    ffmpeg = require('..');
+  fs = require('fs'),
+  shotcut = require('..');
 
 it('should find ffmpeg', function(t) {
-  var stats = fs.statSync(ffmpeg.path);
-  t.ok(stats.isFile(ffmpeg.path));
+  var stats = fs.statSync(shotcut.ffmpeg);
+  t.ok(stats.isFile(shotcut.ffmpeg));
+  t.end();
+});
+
+it('should find ffprobe', function(t) {
+  var stats = fs.statSync(shotcut.ffprobe);
+  t.ok(stats.isFile(shotcut.ffprobe));
+  t.end();
+});
+
+it('should find ffplay', function(t) {
+  var stats = fs.statSync(shotcut.ffplay);
+  t.ok(stats.isFile(shotcut.ffplay));
+  t.end();
+});
+
+it('should find melt', function(t) {
+  var stats = fs.statSync(shotcut.melt);
+  t.ok(stats.isFile(shotcut.melt));
+  t.end();
+});
+
+it('should find qmelt', function(t) {
+  var stats = fs.statSync(shotcut.qmelt);
+  t.ok(stats.isFile(shotcut.qmelt));
+  t.end();
+});
+
+it('should find shotcut', function(t) {
+  var stats = fs.statSync(shotcut.shotcut);
+  t.ok(stats.isFile(shotcut.shotcut));
   t.end();
 });
